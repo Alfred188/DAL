7321cf5e43ceebf95c3f1ded618b9341b13d944d
Alfred188 <Mohsen.mahmoudjanlou@gmail.com>
2018-12-29 20:21:07 +0330

    Remove unnecessary usings

--- DAL.cs
+++ DAL.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
-using System.Linq;
-using System.Web;
 using System.Windows.Forms;
 
 namespace IGNco.Helper
